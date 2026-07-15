e31d3cc6c1d476fdbfab3ba0a9593fddd0acf5ef
Muhammad Taha <mtahaarshad03@gmail.com>
2026-07-15 21:00:24 +0500

    changing again

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             try {
                 // Standard Fetch call (n8n Webhook node par CORS properly allow kiya ho)
-                const response = await fetch('https://tom321.app.n8n.cloud/webhook/3a7a770d-1ea2-492f-a3c3-83f3703e1841', {
+                const response = await fetch('https://tom321.app.n8n.cloud/webhook-test/3a7a770d-1ea2-492f-a3c3-83f3703e1841', {
                     method: 'POST',
                     headers: { 
                         'Content-Type': 'application/json' 
